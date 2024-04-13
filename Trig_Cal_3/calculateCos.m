ea3c6d7bdefbd20c4ca6b05ff8083828db097d2d
hewaside <165648266+hewaside@users.noreply.github.com>
2024-04-13 15:50:24 +0800

    Delete Trig_Cal_3 directory

--- Trig_Cal_3/calculateCos.m
+++
@@ -1,9 +0,0 @@
-function cos_val = calculateCos(rad, n_terms, unit)
-    cos_val = 0;
-    for i = 0:n_terms
-        sign = (-1)^(i);
-        fact = factorial(2*i);
-        cos_val = cos_val + sign * rad^(2*i)/ fact;
-    end
-    cos_val = round(cos_val * unit) / unit;
-end
