8c19f73b115c3402c7e9459d37423b7a56a016b3
tars1111 <154513480@qq.com>
2024-05-16 16:53:12 +0800

    Create calculateArccos.m

---
+++ GUI2.0_by_g12/calculateArccos.m
@@ -0,0 +1,25 @@
+function arccos_val = calculateArccos(x)
+     % 构建反正弦函数在x=0处的泰勒级数
+    
+
+    if x==1
+        arccos_val=0;
+        
+    elseif x==-1
+        arccos_val=pi;
+        
+    else
+        arcsin_val = 0;
+        for i = 0:50
+        factorial_term1 = factorial(2*i);
+        factorial_term2 = factorial(i);
+        term = x^(2*i + 1) * factorial_term1 / (2*i+1) / ((factorial_term2)^2) /(4^i);
+        arcsin_val = arcsin_val + term;
+        end
+        arcsin_val = round(arcsin_val * 1000) / 1000;
+        arccos_val = pi/2 - arcsin_val;
+        arccos_val = round(arccos_val * 1000) / 1000;
+    end
+   
+    
+end
